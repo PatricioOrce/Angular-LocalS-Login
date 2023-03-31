619a1a4a98d1914d4793e48c31a5252fbd8d8605
Patricio <patodotaop@hotmail.com>
2023-03-30 22:35:30 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule
